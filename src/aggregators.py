fix call of tf.nn.softmax

src/aggregators.py
@@ -43,7 +43,7 @@ class Aggregator(object):
 
         # [batch_size, -1, n_neighbor]
         user_relation_scores = tf.reduce_mean(user_embeddings * neighbor_relations, axis=-1)
-        user_relation_scores_normalized = tf.nn.softmax(user_relation_scores, axis=-1)
+        user_relation_scores_normalized = tf.nn.softmax(user_relation_scores, dim=-1)
 
         # [batch_size, -1, n_neighbor, 1]
         user_relation_scores_normalized = tf.expand_dims(user_relation_scores_normalized, axis=-1)