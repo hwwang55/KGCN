669d3f2478d2e7f516318d01661aae74b5f5c89f
hwwang <wanghongwei55@gmail.com>
2018-08-02 18:10:35 +0800

    modify default settings

--- src/main.py
+++ src/main.py
@@ -7,18 +7,48 @@ np.random.seed(555)
 
 
 parser = argparse.ArgumentParser()
+
+# movie
+parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
+parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use,')
+parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
+parser.add_argument('--neighbor_sample_size', type=int, default=4, help='the number of neighbors to be sampled')
+parser.add_argument('--dim', type=int, default=32, help='dimension of user and entity embeddings')
+parser.add_argument('--n_iter', type=int, default=2, help='number of iterations when computing entity representation')
+parser.add_argument('--batch_size', type=int, default=65536, help='batch size')
+parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of l2 regularization')
+parser.add_argument('--lr', type=float, default=0.02, help='learning rate')
+parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
+
+# book
+'''
+parser.add_argument('--dataset', type=str, default='book', help='which dataset to use')
+parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use,')
+parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
+parser.add_argument('--neighbor_sample_size', type=int, default=8, help='the number of neighbors to be sampled')
+parser.add_argument('--dim', type=int, default=64, help='dimension of user and entity embeddings')
+parser.add_argument('--n_iter', type=int, default=1, help='number of iterations when computing entity representation')
+parser.add_argument('--batch_size', type=int, default=256, help='batch size')
+parser.add_argument('--l2_weight', type=float, default=2e-5, help='weight of l2 regularization')
+parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
+parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
+'''
+
+# music
+'''
 parser.add_argument('--dataset', type=str, default='music', help='which dataset to use')
 parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use,')
 parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
 parser.add_argument('--neighbor_sample_size', type=int, default=8, help='the number of neighbors to be sampled')
-parser.add_argument('--dim', type=int, default=32, help='dimension of user and entity embeddings')
+parser.add_argument('--dim', type=int, default=16, help='dimension of user and entity embeddings')
 parser.add_argument('--n_iter', type=int, default=1, help='number of iterations when computing entity representation')
-parser.add_argument('--batch_size', type=int, default=256, help='batch size')
-parser.add_argument('--l2_weight', type=float, default=0.0001, help='weight of l2 regularization')
-parser.add_argument('--lr', type=float, default=0.0005, help='learning rate')
+parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
+parser.add_argument('--lr', type=float, default=5e-4, help='learning rate')
 parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
-args = parser.parse_args()
+'''
 
+args = parser.parse_args()
 show_loss = False
 data = load_data(args)
 train(args, data, show_loss)
