a97aba303b582b82a30d0dfdd1774e981ac86698
hongwei <wanghongwei55@gmail.com>
2019-01-29 00:46:02 +0800

    update parameter settings

--- src/main.py
+++ src/main.py
@@ -65,6 +65,7 @@ parser.add_argument('--batch_size', type=int, default=65536, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of l2 regularization')
 parser.add_argument('--ls_weight', type=float, default=1.0, help='weight of LS regularization')
 parser.add_argument('--lr', type=float, default=2e-2, help='learning rate')
+parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 
 '''
 # book
@@ -78,9 +79,10 @@ parser.add_argument('--batch_size', type=int, default=256, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=2e-5, help='weight of l2 regularization')
 parser.add_argument('--ls_weight', type=float, default=0.5, help='weight of LS regularization')
 parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
+parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 '''
 
-''''
+'''
 # music
 parser.add_argument('--dataset', type=str, default='music', help='which dataset to use')
 parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
@@ -92,6 +94,7 @@ parser.add_argument('--batch_size', type=int, default=128, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
 parser.add_argument('--ls_weight', type=float, default=0.1, help='weight of LS regularization')
 parser.add_argument('--lr', type=float, default=5e-4, help='learning rate')
+parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 '''
 
 '''
@@ -106,11 +109,12 @@ parser.add_argument('--batch_size', type=int, default=65536, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of l2 regularization')
 parser.add_argument('--ls_weight', type=float, default=0.5, help='weight of LS regularization')
 parser.add_argument('--lr', type=float, default=2e-2, help='learning rate')
+parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 '''
 
 show_loss = False
 show_time = False
-show_topk = True
+show_topk = False
 
 t = time()
 
